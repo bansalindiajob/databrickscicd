97b2bc3f76d97869b27f14a6a47be6e3cfbf51a8
bansalindiajob <bansalindiajob@gmail.com>
2020-12-04 16:50:08 +0530

    Delete CICD.py

--- Notebook/CICD.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-spark.conf.set(
-  "fs.azure.account.key.kronosdatabricks.dfs.core.windows.net",
-  "ChpXel84Sor4ZZ6N9DO+dP/H8JxtCFDkCGhYuKm0SsmWkI3JLo03j7MTaSJAfA6GBOAWvRyBAbkwW95uohBbrg=="
-)
-
-# COMMAND ----------
-
-dbutils.fs.ls("abfss://product@kronosdatabricks.dfs.core.windows.net")
